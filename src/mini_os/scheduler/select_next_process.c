95dd85b4e606e604909183436c39ded793494571
jan-kir-wwu <56630012+jan-kir-wwu@users.noreply.github.com>
2022-12-24 14:15:27 +0100

    select next process in queue

--- src/mini_os/scheduler/select_next_process.c
+++ src/mini_os/scheduler/select_next_process.c
@@ -1,17 +1,21 @@
 #include "../mini_os.h"
-
+#include <stdio.h>
 struct process *select_next_process(void)
 {
 	struct process *sel;
 
 	sel = ptable.run_queue.head;
 
+	printf("-------- run_queue HEAD is \"%s\" (PID %i)!\n",
+		   sel->name, sel->process_id);
+
 	for (struct process *cur = sel->next; cur; cur = cur->next)
 	{
-		if (cur->process_id > sel->process_id)
-		{
-			sel = cur;
-		}
+		sel = cur;
+		printf("-------- Process \"%s\" (PID %i) has been selected for "
+			   "the run_queue!\n",
+			   cur->name, cur->process_id);
+		break;
 	}
 
 	queue_remove(sel);
